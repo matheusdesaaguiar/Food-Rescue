a6efc797bcc9213edb925477dba872b5ca8a03b9
Daiane <daihbarbosa16@gmail.com>
2025-05-13 20:13:16 -0300

    feat: :construction: create routes impact

---
+++ src/controller/impactController.js
@@ -0,0 +1,18 @@
+import impactService from '../services/impactService.js';//precisa criar ainda
+
+//falta implementar a lógica de cada função, mas a estrutura está pronta das rotas
+const createReport = async (req, res) => {};
+const getReportById = async (req, res) => {};
+const updateReport = async (req, res) => {};
+const deleteReport = async (req, res) => {};
+const getGlobalImpact = async (req, res) => {};
+const calculateEquivalence = async (req, res) => {};
+
+export default {
+  createReport,
+  getReportById,
+  updateReport,
+  deleteReport,
+  getGlobalImpact,
+  calculateEquivalence,
+};
