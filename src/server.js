e58922aa9767411d592a441a8a8197fc6e2a024b
Millena-Monteiro <707millenamonteiro@gmail.com>
2025-05-19 20:08:57 -0300

    feat(server): conectar rotas de doadores

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import bodyParser from 'body-parser';  // PARA LIDAR COM O BODY DAS REQUISIÇÕES
+import donorRoutes from '../src/routes/donor.routes.js';
+
+
 
 const app = express();
+app.use(bodyParser.json());// "Middleware para permitir que o Express entenda requisições em JSON"
+
+// 🚀 CONECTANDO AS ROTAS DE DOADORES
+app.use('/donors', donorRoutes);
+
+
 const PORT = 3000;
 
 app.listen(PORT, ()=>{
