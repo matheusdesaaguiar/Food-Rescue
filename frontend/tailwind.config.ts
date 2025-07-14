1e5251cd5b0d181aa4331c357a6f7cadc918837a
silascorreia <silas8_silva@hotmail.com>
2025-07-14 08:13:01 -0300

    build: Layout

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -8,15 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      // Aqui definimos a nossa paleta de cores customizada
+      // paleta de cores customizada
       colors: {
-        'background': 'rgb(239, 227, 194)', // Nosso bege/creme
-        'text-dark': 'rgb(18, 53, 36)',      // Nosso verde escuro para textos
+        'background': 'rgb(239, 227, 194)', // bege/creme
+        'text-dark': 'rgb(18, 53, 36)',      // verde escuro para textos
         'primary': 'rgb(62, 123, 39)',       // Verde principal para botões e destaques
         'accent': 'rgb(133, 169, 71)',      // Verde mais claro para acentos visuais
       },
       fontFamily: {
-        // Vamos usar uma fonte mais moderna. Poppins é uma ótima escolha.
+        //  fonte Poppins
         sans: ['Poppins', 'sans-serif'],
       },
     },
